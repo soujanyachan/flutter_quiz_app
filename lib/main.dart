5a34df93a8a893ddd62c3d93cc1633fb647068b5
soujanyac <soujanya@yellowmessenger.com>
2020-05-26 17:20:00 +0530

    added next previous, colour changes and end page

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,12 @@ import 'package:html_unescape/html_unescape.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:core';
+import 'package:crypto/crypto.dart';
 
 // final is a runtime constant
 // const is compile time constant
-// TODO: make a widget for the dropdowns
-// TODO: randomise the answers
 // TODO: next previous question
+// TODO: score calc
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -19,8 +19,8 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _quizData = {};
+  var qNum = 0;
   var _quizDataUpdated = false;
-  var questions = ['fav color?', 'fav series?'];
   var categoryList = <String>['Any', 'Science: Computers', 'Sports', 'Mythology', 'History', 'Politics'];
   var difficultyList = <String>['Any','Easy','Medium','Hard'];
   var typeList = <String>['Any','Multiple Choice', 'True / False'];
@@ -29,7 +29,20 @@ class _MyAppState extends State<MyApp> {
   var dropdownValueType = 'Any';
   var numQuestions = 10;
   var unescape = new HtmlUnescape();
+  var ansNormalColor = 0xff0091EA;
+  var correctAnswer = false;
+  var hasUserSelectedAns = false;
+  var firstClick = true;
+  var endPage = false;
 
+  String generateMd5(String input) {
+    return md5.convert(utf8.encode(input)).toString();
+  }
+
+  List shuffle(List items) {
+    items.sort((a,b) => generateMd5(a.child.child.data).compareTo(b.child.child.data));
+    return items;
+  }
 
   void fetchQuiz() {
     final response =  http.get('https://opentdb.com/api.php?amount=10');
@@ -46,18 +59,12 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: (_quizData.isEmpty) ? Text('Select your quiz parameters:') : Text('Quiz Time!'),
-          ),
-        body:  !_quizDataUpdated ?
-        Container(
+  selectHomeWidget() {
+    if(!_quizDataUpdated) {
+      return Container(
           padding: const EdgeInsets.all(40.0),
           child: Column(
-              children: <Widget> [
+              children: <Widget>[
                 Text(
                   'Number of questions:',
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
@@ -85,7 +92,8 @@ class _MyAppState extends State<MyApp> {
                       dropdownValueCategory = newValue;
                     });
                   },
-                  items: categoryList.map<DropdownMenuItem<String>>((String value) {
+                  items: categoryList.map<DropdownMenuItem<String>>((
+                      String value) {
                     return DropdownMenuItem<String>(
                       value: value,
                       child: Text(value),
@@ -103,7 +111,8 @@ class _MyAppState extends State<MyApp> {
                       dropdownValueDifficulty = newValue;
                     });
                   },
-                  items: difficultyList.map<DropdownMenuItem<String>>((String value) {
+                  items: difficultyList.map<DropdownMenuItem<String>>((
+                      String value) {
                     return DropdownMenuItem<String>(
                       value: value,
                       child: Text(value),
@@ -134,75 +143,146 @@ class _MyAppState extends State<MyApp> {
                     fetchQuiz();
                   },
                 ),
-            ]
-        )
-        )
-            : ( _quizData.isNotEmpty ?
-        Container(
-          width: double.infinity,
-          padding: const EdgeInsets.all(40.0),
-          child: Column(
-              children: [
-                Text(unescape.convert(_quizData['results'][0]['question']), style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    ButtonTheme(height: 12, child: FlatButton(child: Text(_quizData['results'][0]['category'], style: TextStyle(fontSize: 12),), color: Color.fromRGBO(240, 20, 20, 0.5), onPressed: () {},)),
-                    ButtonTheme(height: 12, child: FlatButton(child: Text(_quizData['results'][0]['difficulty'], style: TextStyle(fontSize: 12),), color: Color.fromRGBO(240, 20, 20, 0.5), onPressed: () {},)),
-                  ],
-                ),
-                ButtonTheme(
-                  minWidth: double.infinity,
-                  child: RaisedButton(
-                          child: Text(_quizData['results'][0]['correct_answer'], style: TextStyle(fontSize: 16)),
-                          onPressed: () {print('correct');},
-                          color: Color(0xff0091EA),
-                      ),
-                ),
-                ..._quizData['results'][0]['incorrect_answers'].map((ans) {
-                  return ButtonTheme(
-                    minWidth: double.infinity,
-                    child: new RaisedButton(
-                              child: Text(ans, style: TextStyle(fontSize: 16)),
-                              onPressed: () { print(ans);},
-                              color: Color(0xff0091EA),
-                            ),
-                  );
-                }).toList(),
-              ],
-          ),
-        ) : Column(
-            children: [
-              Text('quiz data empty'),
-              RaisedButton(
-                child: Text('Done'),
-                onPressed: () {
-                  setState(() {
-                    _quizDataUpdated = !_quizDataUpdated;
-                  });
-                },
-              )
-            ]
-        )
-        ),
-        persistentFooterButtons: [
-          Container(
-            child: Row(
+              ]
+          )
+      );
+    } else if ( _quizData.isNotEmpty) {
+      return Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
+          children: [
+            Text(unescape.convert(_quizData['results'][qNum]['question']),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),),
+            Row(
+              mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                RaisedButton(
-                  child: Text('Previous Question'),
-                  onPressed: () {},
-                ),
-                RaisedButton(
-                  child: Text('Next Question'),
-                  onPressed: () {},
-                )],
+                ButtonTheme(height: 12,
+                    child: FlatButton(child: Text(
+                      _quizData['results'][qNum]['category'],
+                      style: TextStyle(fontSize: 12),),
+                      color: Color.fromRGBO(240, 20, 20, 0.5),
+                      onPressed: () {},)),
+                ButtonTheme(height: 12,
+                    child: FlatButton(child: Text(
+                      _quizData['results'][qNum]['difficulty'],
+                      style: TextStyle(fontSize: 12),),
+                      color: Color.fromRGBO(240, 20, 20, 0.5),
+                      onPressed: () {},)),
+              ],
             ),
+            ...shuffle([
+              ButtonTheme(
+                minWidth: double.infinity,
+                child: RaisedButton(
+                  child: Text(unescape.convert(
+                      _quizData['results'][qNum]['correct_answer']),
+                      style: TextStyle(fontSize: 16)),
+                  onPressed: () {
+                    print('correct');
+                    print(firstClick);
+                    if (firstClick) {
+                      setState(() {
+                        hasUserSelectedAns = true;
+                        correctAnswer = true;
+                        firstClick = false;
+                      });
+                    }
+                  },
+                  color: firstClick ? Color(ansNormalColor) : Colors.green,
+                ),
+              ),
+              // if user not clicked everything blue, if user clicked then secondary colours
+              ..._quizData['results'][qNum]['incorrect_answers'].map((ans) {
+                return ButtonTheme(
+                  minWidth: double.infinity,
+                  child: new RaisedButton(
+                    child: Text(unescape.convert(ans), style: TextStyle(
+                        fontSize: 16)),
+                    onPressed: () {
+                      print(ans);
+                      print(firstClick);
+                      if (firstClick) {
+                        setState(() {
+                          hasUserSelectedAns = true;
+                          correctAnswer = false;
+                          firstClick = false;
+                        });
+                      }
+                    },
+                    color: firstClick ? Color(ansNormalColor) : Colors.red,
+                  ),
+                );
+              }).toList(),
+            ]),
+          ],
+        ),
+      );
+    } else if(_quizData.isEmpty){
+      return Column(
+        children: [
+          Text('quiz data empty'),
+          RaisedButton(
+            child: Text('Done'),
+            onPressed: () {
+              setState(() {
+                _quizDataUpdated = !_quizDataUpdated;
+              });
+            },
           )
-        ],
-        bottomNavigationBar: (_quizData.isNotEmpty) ? RaisedButton(
+        ]
+    );
+    } else if (endPage) {
+      return Text('you did it! you made it to the end!');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: (_quizData.isEmpty) ? Text('Select your quiz parameters:') : Text('Quiz Time!'),
+          ),
+        body:  selectHomeWidget(),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(left: 31.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: FloatingActionButton.extended(onPressed: () {
+                  setState(() {
+                    if(qNum > 0) {
+                      qNum--;
+                      hasUserSelectedAns = false;
+                      correctAnswer = false;
+                      firstClick = true;
+                    }
+                  });
+                }, label: Text('prev'), heroTag: null,),
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: FloatingActionButton.extended(onPressed: () {
+                  setState(() {
+                    if (qNum < numQuestions - 1) {
+                      qNum++;
+                      hasUserSelectedAns = false;
+                      correctAnswer = false;
+                      firstClick = true;
+                    } else {
+                      endPage = true;
+                    }
+                  });
+                }, label: Text('next'), heroTag: null,),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: (endPage) ? RaisedButton(
           child: Text('Exit Quiz'),
           onPressed: () {
             setState(() {
